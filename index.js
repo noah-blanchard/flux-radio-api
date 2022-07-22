ed65e3f12bd4236dbc542457de2ed1f865c463f5
Noah <matanaworgen@gmail.com>
2022-07-22 21:17:51 +0200

    implented caching

--- index.js
+++ index.js
@@ -2,12 +2,23 @@
 const cheerio = require("cheerio");
 const axios = require("axios");
 const url = "http://fluxradios.blogspot.com/p/flux-radios-francaise.html";
+const cache = require("memory-cache");
 
 const express = require("express");
 const app = express();
 const port = 3320;
 
-// get endpoints
+const updateCache = async (content) => {
+  cache.clear();
+  cache.put("radios", content, 1000 * 60 * 60);
+}
+
+//update cache every hour
+setInterval(async () => {
+  console.log("Cache updated automatically at : " + new Date());
+  updateCache(await getAll());
+}
+  , 1000 * 60 * 60);
 
 app.get("/", async (req, res) => {
   res.send(await getAll());
@@ -26,8 +37,14 @@ app.get("/:year/:num/:htm", async (req, res) => {
 // get endpoints functions
 
 async function getAll() {
+
+  let radios = cache.get("radios");
+  if (radios.length > 0) {
+    return radios;
+  }
+  console.log("Cache not found, fetching from internet and updating cache : " + new Date());
+  radios = [];
   const $ = cheerio.load(await fetchAll());
-  const radios = [];
   $(".post-body a").each(function (i, elem) {
     if (!($(elem).text().length === 0) && !($(elem).attr("href")[0] === "#")) {
       radios.push({
@@ -37,18 +54,25 @@ async function getAll() {
       });
     }
   });
+  await updateCache(radios);
   return radios;
 }
 
 async function getByTitle(title) {
-  const $ = cheerio.load(await fetchAll());
-  const radios = await getAll();
+  const radios = cache.get("radios");
+  if (!radios) {
+    console.log("Cache not found, fetching from internet and updating cache : " + new Date());
+    const $ = cheerio.load(await fetchAll());
+    radios = await getAll();
+    await updateCache(radios);
+  }
   return radios.filter((radio) => radio.title.toLowerCase().includes(title.toLowerCase()));
+
 }
 
 async function getRadioInfos(link, path) {
   const $ = cheerio.load(await fetchRadio(link));
-  return { quality: "unkown", link: ($("tr > td > span > span").first().text()).replace(/\s/g, ""), path: path, title: ($(".post-body >  table > tbody > tr > td > b > span > span").first().text()).replace(/\s/g, "")};
+  return { quality: "unkown", link: ($("tr > td > span > span").first().text()).replace(/\s/g, ""), path: path, title: ($(".post-body >  table > tbody > tr > td > b > span > span").first().text()).replace(/\s/g, "") };
 }
 
 // axios fetch to get the content of the page
